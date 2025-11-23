f2598c7b6fee5cf608031ea7854c53672f2d6313
Kevin Chemmanathel <63903331+457crash@users.noreply.github.com>
2025-11-23 12:10:18 +0530

    Jj

--- basic_script.py
+++ basic_script.py
@@ -1,3 +1,3 @@
-    a = Tensor([[1, 2], [3, 4]])
-    b = Tensor([[5, 6], [7, 8]])
-    print(a.matmul(b).numpy())
+a = Tensor([[1, 2], [3, 4]])
+b = Tensor([[5, 6], [7, 8]])
+print(a.matmul(b).numpy())
